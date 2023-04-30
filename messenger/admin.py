7dffe24848986504722cbced24fea6b426c448a7
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-05-01 01:22:44 +0600

    messenger: registered message model to admin site
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from messenger.models import Chat
+from messenger.models import Chat, Message
 
 # Register your models here.
 admin.site.register(Chat)
+admin.site.register(Message)
